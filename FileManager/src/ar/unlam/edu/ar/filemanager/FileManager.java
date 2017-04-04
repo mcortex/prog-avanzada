a62468c3f4ec313aaf090858dec7a7968705c05d
Martin Cortez <cortezmartin1983@gmail.com>
2017-04-04 00:44:11 -0300

    Se agrega manejo de vectores

--- FileManager/src/ar/unlam/edu/ar/filemanager/FileManager.java
+++ FileManager/src/ar/unlam/edu/ar/filemanager/FileManager.java
@@ -14,37 +14,48 @@ public class FileManager {
 		//SCANNER: Lectura de archivos
 		String path = "C:\\Users\\Martin\\Documents\\UNLAM\\PrograAvanzada\\Lotes\\PruebaArchivos\\";
 		File archivo = new File(path+"archivo.in");
-		int cantIn=0;
+//		int valor=0;
+
 		
 		try {
 			Scanner scan = new Scanner(archivo);
 			scan.useLocale(Locale.ENGLISH);
-			while(scan.hasNextInt()){
-				int numero=scan.nextInt();
-				cantIn++;
-				System.out.println("Numero: "+numero);
+			int [] vector = new int[scan.nextInt()];
+			
+//			while(scan.hasNextInt()){
+//				valor=scan.nextInt();
+//				System.out.println("Numero: "+valor);
+//				cantIn++;
+//			}
+			
+			for (int i=0;i<vector.length;i++){
+				vector[i]=scan.nextInt();
+				System.out.println("Valor "+i+": "+vector[i]);
 			}
+			
 			scan.close();
+			
+			//PRINT WRITER: Escritura de archivo
+			try {
+				FileWriter writer = new FileWriter(path+"archivo.out");
+				PrintWriter printer = new PrintWriter(writer);
+				
+				printer.println("Cantidad de valores en el archivo de entrada: "+vector.length);
+				
+				for (int i=0;i<vector.length;i++){
+				printer.println(vector[i]);	
+				}
+								
+				printer.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		
-		//PRINT WRITER
-		try {
-			FileWriter writer = new FileWriter(path+"archivo.out");
-			PrintWriter printer = new PrintWriter(writer);
-			//for (int i=0;i<cantIn;i++){
-				
-			//}
-			printer.println("Cantidad de valores en el archivo de entrada: "+cantIn);
-			
-			printer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
+
 	}
 
 }
